b34d047baea94213a2a270e2f07a8080c25d3bbf
mhakimsaputra17 <a1011201296@student.untan.ac.id>
2025-01-18 00:31:12 +0700

    change content desc in layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
         <>
             <Head>
                 <title>PdfImg Tools</title>
-                <meta name='description' content="Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform." />
+                <meta name='description' content="PdfImageTools by mhakimsaputra - Convert your images to PDF easily with our free online tool." />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
